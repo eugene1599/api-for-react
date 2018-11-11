308a8675ee50dae9f0e170fc070ab1d6610e8bb3
reebok159 <reebok159@bk.ru>
2018-11-11 19:45:47 +0200

    disable test validation of user name

--- spec/models/note_spec.rb
+++ spec/models/note_spec.rb
@@ -4,8 +4,4 @@ RSpec.describe Note, type: :model do
   describe 'ActiveRecord associations' do
     it { is_expected.to belong_to(:user) }
   end
-
-  describe 'validations' do
-    xit { is_expected.to validate_presence_of(:title) }
-  end
 end
